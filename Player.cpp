3ecc9e4d5480a954ab3f9c6c22bfb59074dd946d
Sekiraw <90633951+Sekiraw@users.noreply.github.com>
2022-01-27 21:31:48 +0100

    Fixed animation for all directions

--- Player.cpp
+++ Player.cpp
@@ -22,8 +22,8 @@ MyPlayer::MyPlayer(float x, float y, sf::Texture& texture_sheet)
 	this->createMovementComponent(300.f, 14.f, 8.f);
 	this->createAnimationComponent(texture_sheet);
 
-	this->animationComponent->addAnimation("IDLE_LEFT", 10.f, 0, 0, 13, 0, 192, 192);
-	this->animationComponent->addAnimation("WALK_LEFT", 10.f, 0, 1, 11, 1, 192, 192);
+	this->animationComponent->addAnimation("IDLE", 10.f, 0, 0, 13, 0, 192, 192);
+	this->animationComponent->addAnimation("WALK", 10.f, 0, 1, 11, 1, 192, 192);
 }
 
 MyPlayer::~MyPlayer()
@@ -35,14 +35,31 @@ void MyPlayer::update(const float& dt)
 {
 	
 	this->movementComponent->update(dt);
+	//258
 
 	if (this->movementComponent->getState(IDLE)) 
 	{
-		this->animationComponent->play("IDLE_LEFT", dt);
+		this->animationComponent->play("IDLE", dt);
 	}
 	else if(this->movementComponent->getState(MOVING_LEFT))
 	{
-		this->animationComponent->play("WALK_LEFT", dt);
+		this->sprite.setOrigin(0, 0.f); //Flip
+		this->sprite.setScale(1.f, 1.f);
+		this->animationComponent->play("WALK", dt);
+	}
+	else if (this->movementComponent->getState(MOVING_RIGHT)) 
+	{
+		this->sprite.setOrigin(258.f, 0.f);
+		this->sprite.setScale(-1.f, 1.f);
+		this->animationComponent->play("WALK", dt);
+	}
+	else if (this->movementComponent->getState(MOVING_UP)) {
+		this->animationComponent->play("WALK", dt);
+
+	}
+	else if (this->movementComponent->getState(MOVING_DOWN)) {
+		this->animationComponent->play("WALK", dt);
+
 	}
 
 	this->hitboxComponent->update();
