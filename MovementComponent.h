c35c31583f4d4ba6e8b0d3a93c1b002c8d431c5c
Sekiraw <90633951+Sekiraw@users.noreply.github.com>
2022-01-27 14:37:20 +0100

    Fixing animation directions and states
    Not all directions

--- MovementComponent.h
+++ MovementComponent.h
@@ -19,6 +19,9 @@
 #include"SFML/System.hpp"
 #include"SFML/Audio.hpp"
 #include"SFML/Network.hpp"
+
+enum movement_states { IDLE = 0, MOVING, MOVING_LEFT, MOVING_RIGHT, MOVING_UP, MOVING_DOWN };
+
 class MovementComponent
 {
 private:
@@ -42,7 +45,7 @@ public:
 	const sf::Vector2f& getVelocity() const;
 
 	//Functions
-	const bool idle() const;
+	const bool getState(const short unsigned state) const;
 
 	void move(const float dir_x, const float dir_y, const float& dt);
 	void update(const float& dt);
